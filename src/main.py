10c956d1d28daa6b7ba7681e0b1f738d24b7bacd
JMar22 <juan.martinez.2488@gmail.com>
2025-05-30 14:50:40 -0700

    added build script

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import os
 import shutil
+import sys
+from tarfile import NUL
 
 from copystatic import copy_files_recursive
 from gencontent import generate_pages_recursive
@@ -7,6 +9,7 @@ from gencontent import generate_pages_recursive
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+dir_path_docs = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 
@@ -19,8 +22,10 @@ def main():
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
+    basePath = os.path.dirname(os.path.abspath(sys.argv[0]))
+
     print("Generating content...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_docs, basePath)
 
 
 main()
